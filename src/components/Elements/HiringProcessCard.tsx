56e4f0cb38fb8e9a9629d518c3b960c9b3dcaa51
Sourav <souravbhardwaj997@gmail.com>
2025-11-18 22:44:15 +0530

    Add Hiring Cards

---
+++ src/components/Elements/HiringProcessCard.tsx
@@ -0,0 +1,33 @@
+import { RightArrow } from "../icons/Arrow";
+
+export const HiringProcessCard = ({
+  title,
+  description,
+  icon,
+  step,
+  showRightArrow = true,
+}: {
+  title: string;
+  description: string;
+  icon?: React.ReactNode;
+  step: string;
+  showRightArrow?: boolean;
+}) => {
+  return (
+    <div className="flex flex-col items-center gap-3 relative">
+      <div className="border-8 border-primary rounded-full size-44 flex justify-center items-center bg-white relative">
+        {icon}
+        <div className="size-15 bg-secondary border-3 border-white rounded-full text-white flex justify-center items-center text-3xl font-semibold absolute -top-4 right-0 ">
+          {step}
+        </div>
+      </div>
+      {showRightArrow && (
+        <div className="absolute top-20 -right-5">
+          <RightArrow />
+        </div>
+      )}
+      <p className="font-semibold text-2xl">{title}</p>
+      <p className="text-center font-light">{description}</p>
+    </div>
+  );
+};
--- src/components/Sections/HiringProcessSection.tsx
+++ src/components/Sections/HiringProcessSection.tsx
@@ -1,4 +1,33 @@
+import { HiringProcessCard } from "../Elements/HiringProcessCard";
+import { DevMonitorIcon, PersonIcon, ScalingIcon, TeamIcon } from "../icons";
+
 export const HiringProcessSection = () => {
+  const hiringProcessData = [
+    {
+      title: "Inquiry",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      icon: <PersonIcon />,
+    },
+    {
+      title: "Select Developers",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      icon: <DevMonitorIcon />,
+    },
+    {
+      title: "Team Integration",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      icon: <TeamIcon />,
+    },
+    {
+      title: "Team Scaling",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      icon: <ScalingIcon />,
+    },
+  ];
   return (
     <div className="bg-[url('/hiring-process-bg.png')] bg-no-repeat bg-cover">
       <div className="2xl:w-10/12 lg:w-10/12 md:w-5/6 sm:w-full xl:w-11/12 mx-auto py-14 flex flex-col justify-center gap-6 items-center">
@@ -9,6 +38,16 @@ export const HiringProcessSection = () => {
           Take a look at our simple and straightforward process to hire software
           developers from ValueCoders.
         </p>
+        <div className="grid grid-cols-4 mt-8">
+          {hiringProcessData.map((data, i) => (
+            <HiringProcessCard
+              showRightArrow={hiringProcessData.length - 1 !== i}
+              key={i}
+              {...data}
+              step={JSON.stringify(i + 1)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
