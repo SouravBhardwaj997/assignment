b7f687ef40e48d227e81c5d93a2b3e8ce79a347e
Sourav <souravbhardwaj997@gmail.com>
2025-11-19 11:15:02 +0530

    Add TestimonialSection component with layout and content

--- src/components/Sections/TestimonialSection.tsx
+++ src/components/Sections/TestimonialSection.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export const TestimonialSection = () => {
-  return <div className="">TestimonialSection</div>;
+  return (
+    <div className="bg-white">
+      <div className="2xl:w-10/12 lg:w-10/12 md:w-5/6 sm:w-full xl:w-11/12 mx-auto py-16">
+        <div className="w-full flex flex-col gap-8 items-center text-gray-dark">
+          <p className="text-5xl font-semibold">
+            What Our Clients Have to Say About Us
+          </p>
+          <p className="font-light">
+            Take a look at our simple and straightforward process to hire
+            software
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
