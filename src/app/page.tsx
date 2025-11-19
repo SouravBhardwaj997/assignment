ff880663dee1635db99475d5151454d8fd4f327e
Sourav <souravbhardwaj997@gmail.com>
2025-11-19 22:34:45 +0530

    Refactor import statements for Sections in Home component for improved organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { HeroSection, WhyUsSection } from "@/components/Sections";
-import { HiringProcessSection } from "@/components/Sections/HiringProcessSection";
-import { TechSection } from "@/components/Sections/TechSection";
-import { TestimonialSection } from "@/components/Sections/TestimonialSection";
-import { TrustFactorSection } from "@/components/Sections/TrustFactorSection";
-import { UserGuideSection } from "@/components/Sections/UserGuideSection";
-import { WhyHireIndiaSection } from "@/components/Sections/WhyHireIndiaSection";
+import {
+  HeroSection,
+  WhyUsSection,
+  TechSection,
+  TestimonialSection,
+  WhyHireIndiaSection,
+  HiringProcessSection,
+  TrustFactorSection,
+  UserGuideSection,
+} from "@/components/Sections";
 
 export default function Home() {
   return (
--- src/components/Sections/index.ts
+++ src/components/Sections/index.ts
@@ -1,2 +1,8 @@
 export { HeroSection } from "./HeroSection";
 export { WhyUsSection } from "./WhyUsSection";
+export { TechSection } from "./TechSection";
+export { WhyHireIndiaSection } from "./WhyHireIndiaSection";
+export { TrustFactorSection } from "./TrustFactorSection";
+export { UserGuideSection } from "./UserGuideSection";
+export { TestimonialSection } from "./TestimonialSection";
+export { HiringProcessSection } from "./HiringProcessSection";
